c25a4186888184844bec0ce07a1417601aee6755
Your Name <you@example.com>
2022-01-11 13:52:02 +0700

    baitap7

---
+++ js/bai1.js
@@ -0,0 +1,12 @@
+let n = prompt('can bat 2 cua');
+console.log(Math.sqrt(n));
+
+let a = prompt('canh day');
+let h = prompt('chieu cao');
+console.log(' dien tich cua tam giac'+ (Math.sqrt(a * h) /2));
+
+let f = prompt('nhiet do f : ');
+console.log('Nhiet do c la : ' + (f - 32) / 1.8);
+
+let km = prompt('so km ');
+console.log(' bang ' + (km * 1000) + 'm');
